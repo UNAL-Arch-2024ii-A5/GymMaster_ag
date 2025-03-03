50e42f5e0d6e77203ddaadff77e7fc65246d1a72
David Arias <daaariaspa@unal.edu.co>
2025-03-02 20:54:41 -0500

    merge changes

--- machines_ms/mutations.js
+++ machines_ms/mutations.js
@@ -1,42 +1,5 @@
 const axios=require("axios");
 const mutations= {
-<<<<<<< HEAD
-    createMachineMS: async (_, { name, description, state, type, serviceInterval }) => {
-        const response = await axios.post(`${process.env.MACHINES_URL}`, {
-          name,
-          description,
-          state,
-          type,
-          serviceInterval
-        });
-        return response.data;
-      },
-      updateUseMachine: async (_, { id }) => {
-        const response = await axios.put(`${process.env.MACHINES_URL}/use/${id}`); 
-        return response.data;
-      },
-      updateMachineMS: async (_, { id, name, description, state, lastService, serviceInterval }) => {
-        const response = await axios.put(`${process.env.MACHINES_URL}/${id}`, {
-          name,
-          description,
-          state,
-          lastService,
-          serviceInterval,
-        });
-        return response.data;
-      },
-      deleteMachine: async (_, { id }) => {
-        const response = await axios.delete(`${process.env.MACHINES_URL}/${id}`);
-        return response.data;
-      },
-      addMachineService: async (_, { machineId, date, description }) => {
-        const response = await axios.post(`${process.env.MACHINES_URL}/${machineId}/services`, {
-          date,
-          description,
-        });
-        return response.data;
-      }
-=======
   // ✅ SOLO ADMIN puede crear máquinas
   createMachineMS: async (_, { name, description, state, type, lastService, serviceInterval }, userData) => {
     if (userData.role !== "admin") throw new Error("No tienes permisos");
@@ -81,7 +44,6 @@ const mutations= {
     });
     return response.data;
   }
->>>>>>> bfdea008a9259153076af0bf91d0738985ed3276
 };
 
 module.exports = {
